8c49e0325187eee21492736139a817285289a0cc
CheerfulGrape <109738935+CheerfulGrape@users.noreply.github.com>
2023-03-22 13:31:29 +0300

    Dev 1

--- src/main/java/org/bmstu/crawler/Main.java
+++ src/main/java/org/bmstu/crawler/Main.java
@@ -3,16 +3,34 @@ package org.bmstu.crawler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
-    private static Logger log = LogManager.getLogger();
+    private static final Logger log = LogManager.getLogger();
     private static TaskController taskController = null;
-    private static String site = "https://mytishi.ru";
+    private static final String site = "https://dtf.ru/";
+
+    static public void ParseNews(Document doc) {
+        try {
+            Elements news = doc.getElementsByClass("").select("");
+            for (Element element : news) {
+
+            }
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
 
     public static void main(String[] args) {
         taskController = new TaskController(site);
         Document doc = taskController.getUrl(site);
-        System.out.println("Hello world!");
+        String title;
+        if (doc != null) {
+            title = doc.title();
+            log.info(title);
+            ParseNews(doc);
+        }
     }
 
 }
--- src/main/java/org/bmstu/crawler/TaskController.java
+++ src/main/java/org/bmstu/crawler/TaskController.java
@@ -1,5 +1,6 @@
 package org.bmstu.crawler;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -9,17 +10,19 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class TaskController {
-    private static Logger log = LogManager.getLogger();
-    private CloseableHttpClient client = null;
-    private HttpClientBuilder builder;
-    private String server = "https://mytyshi.ru";
-    private int retryDelay = 5 * 1000;
-    private int retryCount = 2;
-    private int metadataTimeout = 30 * 1000;
+import java.io.IOException;
 
+public class TaskController {
+    private static final Logger log = LogManager.getLogger();
+    private CloseableHttpClient client = null;
+    private HttpClientBuilder builder = null;
+    private String server = "https://mytyshi.ru";
+    private final int retryDelay = 5 * 1000;
+    private final int retryCount = 2;
+    private final int metadataTimeout = 30 * 1000;
 
     public TaskController(String _server) {
         CookieStore httpCookieStore = new BasicCookieStore();
@@ -53,11 +56,44 @@ public class TaskController {
                     log.warn("error get url " + url + " code " + code);
                     bStop = true;
                 } else if (code == 200) {
-
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null) {
+                        try {
+                            doc = Jsoup.parse(entity.getContent(), "UTF-8", server);
+                            break;
+                        } catch (IOException e) {
+                            log.error(e);
+                        }
+                    }
+                    bStop = true;
+                } else {
+                    log.warn("error get url " + url + " code " + code);
+                    response.close();
+                    response = null;
+                    client.close();
+                    CookieStore httpCookieStore = new BasicCookieStore();
+                    builder.setDefaultCookieStore(httpCookieStore);
+                    client = builder.build();
+                    int delay = retryDelay * (iTry + 1);
+                    log.info("wait" + delay / 1000 + " s ...");
+                    try {
+                        Thread.sleep(delay);
+                        continue;
+                    } catch (InterruptedException ex) {
+                        break;
+                    }
                 }
-            } catch (...) {
-
+            } catch (IOException e) {
+                log.error(e);
+            }
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    log.error(e);
+                }
             }
         }
+        return doc;
     }
 }
