ac703a84ecfb2dc818bf754ebfea97189dc3f5d6
CheerfulGrape <v.kolka@mail.ru>
2023-05-23 02:37:01 +0300

    Copy-paste needed code

--- src/main/java/org/bmstu/ISaDA/Main.java
+++ src/main/java/org/bmstu/ISaDA/Main.java
@@ -1,4 +1,27 @@
 package org.bmstu.ISaDA;
 
 public class Main {
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("rabbitmq");
+        factory.setPassword("rabbitmq");
+        factory.setVirtualHost("/");
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+
+        try {
+            Connection conn = factory.newConnection();
+            Channel channel = conn.createChannel();
+            taskController = new TaskController(channel);
+        } catch (Exception e) {
+            log.error(e);
+            return;
+        }
+
+        Document doc = taskController.getUrl(site);
+        if (doc != null) {
+            log.info("Обрабатываю страницу \"" + doc.title() + "\"");
+            ParseNews(doc);
+        }
+    }
 }
